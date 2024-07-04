f171a16a0dbf46813b1a7976b442d2aef30c58ee
Jonathan Gin <jonathanegin@gmail.com>
2024-07-04 03:22:38 +0000

    Use Source YAML files

--- models/staging/jaffle_shop/stg_jaffle_shop_customers.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop_customers.sql
@@ -1,11 +1,6 @@
-{{
-config(
-materialized='table'
-)
-}}
-
 select
     id as customer_id,
     first_name,
-    last_name
-from `dbt-tutorial.jaffle_shop.customers`
+    last_name,
+    current_timestamp() as dp_load_date
+from {{ source('jaffle_shop', 'customers') }}
